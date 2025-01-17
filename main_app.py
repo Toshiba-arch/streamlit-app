e334a4e6f460266f302e10b733d4c828a2f43aa8
Toshiba-arch <toshiba.sala@gmail.com>
2025-01-17 17:10:52 +0000

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -2,16 +2,17 @@ import streamlit as st
 from app_ofertas import run as run_ofertas
 from app_euromilhoes import run as run_euromilhoes
 
-st.title("App Principal")
+# Função para o menu de navegação
+def main():
+    st.sidebar.title("Escolha a funcionalidade")
+    
+    # Dropdown com as opções
+    opcao = st.sidebar.selectbox("Selecione a funcionalidade", ["Gerador de Ofertas", "Gerador de Números do Euromilhões"])
 
-# Sidebar com dropdown para selecionar a aplicação
-app_selecionada = st.sidebar.selectbox(
-    "Selecione a aplicação:",
-    ("Gerador de Ofertas", "Gerador de Números do Euromilhões"),
-)
+    if opcao == "Gerador de Ofertas":
+        run_ofertas()  # Chama a funcionalidade de ofertas
+    elif opcao == "Gerador de Números do Euromilhões":
+        run_euromilhoes()  # Chama a funcionalidade de Euromilhões
 
-# Carregar a aplicação selecionada
-if app_selecionada == "Gerador de Ofertas":
-    run_ofertas()
-elif app_selecionada == "Gerador de Números do Euromilhões":
-    run_euromilhoes()
+if __name__ == "__main__":
+    main()
