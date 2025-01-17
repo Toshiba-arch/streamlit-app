b0d1bf5dc192a62401529e9126c7bee6e8f4e162
Toshiba-arch <toshiba.sala@gmail.com>
2025-01-17 18:26:51 +0000

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,16 +1,21 @@
 import streamlit as st
 from app_euromilhoes import run as run_euromilhoes
 from app_ofertas import run as run_ofertas
+from app_matricula import run as run_matricula  # Importando a nova app
 
-# Função para exibir o menu de escolha e chamar a aplicação correta
-def app():
-    st.sidebar.title("Menu de Funcionalidades")
-    opcao = st.sidebar.radio("Selecione uma aplicação", ("Euromilhões", "Promoções"))
+# Título principal da app
+st.title("App de Funcionalidades Diversas")
 
-    if opcao == "Euromilhões":
-        run_euromilhoes()
-    elif opcao == "Promoções":
-        run_ofertas()
+# Criar uma dropdown para selecionar a funcionalidade desejada
+opcao = st.selectbox(
+    "Selecione a funcionalidade",
+    ["Gerador de Números do Euromilhões", "Consultor de Promoções", "Consulta de Marca do Carro pela Matrícula"]
+)
 
-if __name__ == "__main__":
-    app()
+# De acordo com a seleção, chamamos a função correspondente
+if opcao == "Gerador de Números do Euromilhões":
+    run_euromilhoes()
+elif opcao == "Consultor de Promoções":
+    run_ofertas()
+elif opcao == "Consulta de Marca do Carro pela Matrícula":
+    run_matricula()  # Chama a nova app para consulta de matrícula
