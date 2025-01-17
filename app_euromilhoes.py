ed2e5cae8b23e75907a7881561f5de8a4ff91436
Toshiba-arch <toshiba.sala@gmail.com>
2025-01-17 16:30:20 +0000

    Create app_euromilhoes.py

---
+++ app_euromilhoes.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import random
+
+# Funções e código principal para o Gerador de Números do Euromilhões
+def gerar_numeros_euromilhoes(qtd_estrelas, qtd_numeros):
+    # Código relacionado ao gerador de números do Euromilhões
+    # ...
+
+st.title("Gerador de Números do Euromilhões")
+st.sidebar.header("Menu")
+st.sidebar.subheader("Gerar Números")
+
+# Código relacionado à interface do gerador de números
+# ...
