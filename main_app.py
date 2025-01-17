ac97aaac2c5026600d976e16441999a721f147c9
Toshiba-arch <toshiba.sala@gmail.com>
2025-01-17 18:15:59 +0000

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,19 +1,16 @@
 import streamlit as st
 from app_euromilhoes import run as run_euromilhoes
-# Da mesma forma, você pode adicionar outras importações de aplicações se necessário, como:
-# from app_ofertas import run as run_ofertas
+from app_ofertas import run as run_ofertas
 
 # Função para exibir o menu de escolha e chamar a aplicação correta
 def app():
     st.sidebar.title("Menu de Funcionalidades")
-    opcao = st.sidebar.radio("Selecione uma aplicação", ("Euromilhões", "Outra Aplicação"))
+    opcao = st.sidebar.radio("Selecione uma aplicação", ("Euromilhões", "Promoções"))
 
     if opcao == "Euromilhões":
         run_euromilhoes()
-    elif opcao == "Outra Aplicação":
-        # Aqui você pode adicionar a lógica para chamar outras aplicações, se necessário
-        st.title("Outra Aplicação")
-        st.write("Aqui irá a lógica para outra funcionalidade.")
+    elif opcao == "Promoções":
+        run_ofertas()
 
 if __name__ == "__main__":
     app()
