0c484f2c40033a227ac3ac0c293dab540cc0cbd5
Toshiba-arch <toshiba.sala@gmail.com>
2025-01-17 18:12:27 +0000

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,32 +1,19 @@
 import streamlit as st
-from app_ofertas import run as run_ofertas
 from app_euromilhoes import run as run_euromilhoes
+# Da mesma forma, você pode adicionar outras importações de aplicações se necessário, como:
+# from app_ofertas import run as run_ofertas
 
-def main():
-    # Personalizando a largura do menu lateral e título
-    st.markdown(
-        """
-        <style>
-        .css-1d391kg {width: 200px;}  # Ajuste a largura do menu lateral
-        h1 {font-size: 24px;}          # Menor título principal
-        .css-18e3th9 {font-size: 20px;} # Título em uma área específica
-        </style>
-        """, unsafe_allow_html=True)
+# Função para exibir o menu de escolha e chamar a aplicação correta
+def app():
+    st.sidebar.title("Menu de Funcionalidades")
+    opcao = st.sidebar.radio("Selecione uma aplicação", ("Euromilhões", "Outra Aplicação"))
 
-    # Título da aplicação
-    st.header("Selecione a Aplicação")
-
-    # Dropdown para selecionar a aplicação
-    app_selecionada = st.sidebar.selectbox(
-        "Escolha a aplicação",
-        ("App de Ofertas", "App de Euromilhões")
-    )
-
-    # Condicional para escolher qual aplicação executar
-    if app_selecionada == "App de Ofertas":
-        run_ofertas()
-    elif app_selecionada == "App de Euromilhões":
+    if opcao == "Euromilhões":
         run_euromilhoes()
+    elif opcao == "Outra Aplicação":
+        # Aqui você pode adicionar a lógica para chamar outras aplicações, se necessário
+        st.title("Outra Aplicação")
+        st.write("Aqui irá a lógica para outra funcionalidade.")
 
 if __name__ == "__main__":
-    main()
+    app()
