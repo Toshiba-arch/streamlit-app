2adcf17dba3d89bbb1b9ea646e08a2e41f6d1c17
Toshiba-arch <toshiba.sala@gmail.com>
2025-01-17 17:30:25 +0000

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -2,15 +2,27 @@ import streamlit as st
 from app_ofertas import run as run_ofertas
 from app_euromilhoes import run as run_euromilhoes
 
-# Função de execução principal para o menu de seleção
 def main():
-    st.title("Selecione a Aplicação")
+    # Personalizando a largura do menu lateral e título
+    st.markdown(
+        """
+        <style>
+        .css-1d391kg {width: 200px;}  # Ajuste a largura do menu lateral
+        h1 {font-size: 24px;}          # Menor título principal
+        .css-18e3th9 {font-size: 20px;} # Título em uma área específica
+        </style>
+        """, unsafe_allow_html=True)
 
+    # Título da aplicação
+    st.header("Selecione a Aplicação")
+
+    # Dropdown para selecionar a aplicação
     app_selecionada = st.sidebar.selectbox(
         "Escolha a aplicação",
         ("App de Ofertas", "App de Euromilhões")
     )
 
+    # Condicional para escolher qual aplicação executar
     if app_selecionada == "App de Ofertas":
         run_ofertas()
     elif app_selecionada == "App de Euromilhões":
