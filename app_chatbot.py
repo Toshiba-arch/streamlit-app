5a0e4af93660be2d35bf441fe3383831b9cdb2d1
Toshiba-arch <toshiba.sala@gmail.com>
2025-01-24 12:01:55 +0000

    Update app_chatbot.py

--- app_chatbot.py
+++ app_chatbot.py
@@ -8,7 +8,7 @@ def run():
         #"This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
         #"To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
         #"You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-    )
+   #)
 
     # Obter a API Key dos secrets
     openai_api_key = st.secrets.get("openai_api_key")
