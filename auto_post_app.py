e7414878def07bb90729df2dc132361d5b562baa
Toshiba-arch <toshiba.sala@gmail.com>
2025-01-27 11:40:27 +0000

    Update auto_post_app.py

--- auto_post_app.py
+++ auto_post_app.py
@@ -124,10 +124,5 @@ def auto_post_app():
                 st.write("### Compartilhar:")
                 st.button("Compartilhar no Facebook (Simulado)")
 
-            try:
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()  # Isso vai levantar um erro para status 4xx ou 5xx
-    st.write(response.status_code)  # Exiba o status da resposta
-except requests.exceptions.RequestException as e:
-    st.error(f"Erro ao processar o link: {e}")
-
+            except requests.exceptions.RequestException as e:
+                st.error(f"Erro ao processar o link: {e}")
