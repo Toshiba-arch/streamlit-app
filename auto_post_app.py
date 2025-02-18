a8cc8f82633f7ac0116f8fa24762b6c1bca31be6
Toshiba-arch <toshiba.sala@gmail.com>
2025-02-18 19:08:56 +0000

    Update auto_post_app.py

--- auto_post_app.py
+++ auto_post_app.py
@@ -265,17 +265,17 @@ def auto_post_app():
                     </button>
                 </a>
                 <a href="https://www.facebook.com/sharer/sharer.php?u={url_afiliado_encoded}&quote={texto_compartilhamento}" target="_blank">
-                    <button style="background-color: #1877F2; color: white; padding: 8px 16px; border: outset; border-radius: 5px; margin-right: 10px;">
+                    <button style="background-color: #1877F2; color: white; padding: 8px 16px; border: outset; margin-right: 10px;">
                         📘 Facebook
                     </button>
                 </a>
                 <a href="https://api.whatsapp.com/send?text={texto_compartilhamento}%20{url_afiliado_encoded}" target="_blank">
-                    <button style="background-color: #25D366; color: white; padding: 8px 16px; border: outset; border-radius: 5px; margin-right: 10px;">
+                    <button style="background-color: #25D366; color: white; padding: 8px 16px; border: outset; margin-right: 10px;">
                         💬 WhatsApp
                     </button>
                 </a>
                 <a href="https://www.pinterest.com/pin/create/button/?url={url_afiliado_encoded}&description={texto_compartilhamento}&media={url_da_imagem}" target="_blank">
-                    <button style="background-color: #BD081C; color: white; padding: 8px 16px; border: outset; border-radius: 5px; margin-right: 10px">
+                    <button style="background-color: #BD081C; color: white; padding: 8px 16px; border: outset; margin-right: 10px">
                         📌 Pinterest
                     </button>
                 </a>
